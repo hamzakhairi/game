763ef7acb263198027954c19ee29a1c1948f2d18
Hamza Khairi <hkhairi@e1r6p7.1337.ma>
2025-02-19 21:57:12 +0100

    update validation

--- bonus/free_validation.c
+++ bonus/free_validation.c
@@ -6,23 +6,23 @@
 /*   By: hkhairi <hkhairi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/19 20:02:44 by hkhairi           #+#    #+#             */
-/*   Updated: 2025/02/19 20:05:34 by hkhairi          ###   ########.fr       */
+/*   Updated: 2025/02/19 20:29:36 by hkhairi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./so_long.h"
 
-void free_one(t_game	*game, char *str)
+void	free_one(t_game *game, char *str)
 {
-    print_error(str);
+	print_error(str);
 	free_map(game->map);
 	free(game);
 	exit(1);
 }
 
-void    free_tow(t_valid	*sume, t_game	*game, char *str)
+void	free_tow(t_valid *sume, t_game *game, char *str)
 {
-    print_error(str);
+	print_error(str);
 	free(sume);
 	free_map(game->map);
 	free(game);
